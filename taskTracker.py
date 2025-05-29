e63c452119511a9045ac55a0776a369a4f466e08
M0-17 <88941810+M0-17@users.noreply.github.com>
2025-05-29 00:41:17 -0500

    Update taskTracker.py

--- taskTracker.py
+++ taskTracker.py
@@ -1,1 +1,54 @@
-#import pandas as pd
+import pandas as pd
+import tkinter as tk
+
+print("hello world")
+
+root = tk.Tk()
+root.geometry('750x750')
+
+
+# title = tk.Label(text="Task Tracker by Project")
+# title.grid(column=0, row=0)
+
+filename = r'C:\Users\brads\OneDrive\Programs\Task_Tracker_by_Project\example.json'
+
+
+status_values = ['Open', 'In-process', 'Waiting on Responce', 'Closed']
+project_values = ['kitchen', 'bedroom']
+group_values = ['contractor', 'sub']
+
+# df = pd.read_json(filename)
+projects = pd.Series(['',])
+df = pd.DataFrame({'id': pd.Series(dtype='int'), 'name': pd.Series(dtype='str'), 'status': pd.Series(status_values, dtype='category'), 'project': pd.Series(project_values, dtype='category'), 'group': pd.Series(group_values, dtype='category'),      'notes': pd.Series(dtype='str'), 'created': pd.Series(dtype='datetime64[ns]'), 'due': pd.Series(dtype='datetime64[ns]'), 'last_activity': pd.Series(dtype='datetime64[ns]')})
+df['project'] = df['project'].astype('category')
+# 'people': pd.Series(dtype='array'),
+
+
+df['id'].fillna(0)
+df['name'].fillna('')
+
+# print(df.to_string())
+
+for index, row in df.iterrows():
+    # if row['status'] != Closed
+    id_box = tk.Label(root, text=str(index))
+    id_box.grid(row=index, column=0)
+
+    # name_box = tk.Text(root, row['name'], height=2, width=15)
+    name_box = tk.Entry(root)
+    name_box.insert(0, row['name'])
+    name_box.grid(row=index, column=1)
+
+    status_box = tk.OptionMenu(root, row['status'], *status_values)
+    status_box.grid(row=index, column=2)
+
+    
+
+
+print(df.to_string())
+
+
+
+
+
+root.mainloop()
