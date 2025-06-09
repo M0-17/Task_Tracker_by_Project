7834f794cc309d3df5585e678cd3bb55384f0895
M0-17 <88941810+M0-17@users.noreply.github.com>
2025-06-08 22:33:04 -0500

    Update taskTracker.py v1.0

--- taskTracker.py
+++ taskTracker.py
@@ -1,3 +1,5 @@
+# Version 1.0
+
 import pandas as pd
 import tkinter as tk
 from tkinter import ttk
@@ -13,14 +15,7 @@ def select_file():
     filename = filedialog.askopenfilename(initialdir = os.path.dirname(filename), title='Select Filename')
     filename_val.set(filename)
 
-def insert_new_row():
-    col_offset = 0
-    print("new row")
-    global max_uid
-    max_uid = max_uid + 1
-    id = max_uid
-    uid_box[id] = tk.Label(root, text=str(id))
-    uid_box[id].grid(row=root.grid_size()[1], column=col_offset)
+
 
 def update_last_activity(index, uid):
     df.at[index, 'last_activity'] = pd.Timestamp.now()
@@ -65,15 +60,93 @@ def notes_callback(name, index, mode):
 
 def Timestamp_callback(uid, col_label, value):              # TODO Fix this!!!
     index = df.index[df['uid'] == uid].to_list()[0]
-    print(Timestamp_callback)
+    print(index)
+    print(df.to_string())
+
     update_last_activity(index, uid)
 
 def due_callback(name, index, mode):
 
-    Timestamp_callback(name_val[name][1], 'due', name_val[name][0].get())       # TODO Fix this!!!
+    Entity_Combo_callback(due_val[name][1], 'due', due_val[name][0].get())       # TODO Fix this!!!
 
 def last_activity_callback(name, index, mode):
     Timestamp_callback(name_val[name][1], 'last_activity', name_val[name][0].get()) # TODO Fix this!!!
+
+#######################################################################################
+
+def new_StringVar(uid, val_dict, col_name, callback):
+    temp = tk.StringVar()
+    val_dict[temp._name] = (temp, uid)
+    val_dict[temp._name][0].set('')
+    val_dict[temp._name][0].trace('w', callback)
+    return temp
+
+def insert_new_row():
+    global df
+    global max_uid
+    
+    col_offset = 0
+    print("new row")
+    max_uid = max_uid + 1
+    uid = max_uid
+    index = len(df)
+    # ['Select', 'UId', 'Name', 'Status', 'Priority', 'Project', 'Group', 'People', 'Notes', 'Due', 'Last Activity', 'Created']
+    df.loc[index] = {'select': '', 'uid' : uid, 'priority': '', 'project': '', 'group': '', 'people': '', 'notes': '', 'due': '', 'last_activity': pd.Timestamp.now(), 'created': pd.Timestamp.now()}
+
+    # load_row(index=index - 1, row=df.iloc[-1], col_offset=col_offset)
+
+    print(df.to_string())
+
+    
+    checkbox_box[uid] = tk.Checkbutton(root, onvalue=1, offvalue=0)
+    checkbox_box[uid].grid(row=index + 2, column=0 + col_offset)
+
+    uid_box[uid] = tk.Label(root, text=str(uid))
+    uid_box[uid].grid(row=index + 2, column=1 + col_offset)
+
+    temp = new_StringVar(uid, name_val, 'name', name_callback)
+    name_box[uid] = tk.Entry(root, textvariable=name_val[temp._name][0])    
+    name_box[uid].grid(row=index + 2, column=2 + col_offset)
+
+    temp = new_StringVar(uid, status_val, 'status', status_callback)
+    status_box[uid] = ttk.Combobox(root, textvariable=status_val[temp._name][0], width=20, values=status_values, state="readonly")
+    status_box[uid].grid(row=index + 2, column=3 + col_offset)
+
+    temp = new_StringVar(uid, priority_val, 'priority', priority_callback)
+    priority_box[uid] = ttk.Combobox(root, textvariable=priority_val[temp._name][0], width=20, values=priority_values, state="readonly")
+    priority_box[uid].grid(row=index + 2, column=4 + col_offset)
+    
+    temp = new_StringVar(uid, project_val, 'project', project_callback)
+    project_box[uid] = ttk.Combobox(root, textvariable=project_val[temp._name][0], width=20, values=project_values, state="readonly")
+    project_box[uid].grid(row=index + 2, column=5 + col_offset)
+
+    temp = new_StringVar(uid, group_val, 'group', group_callback)
+    group_box[uid] = ttk.Combobox(root, textvariable=group_val[temp._name][0], width=20, values=group_values, state="readonly")
+    group_box[uid].grid(row=index + 2, column=6 + col_offset)
+
+    # people_box =   #TODO
+
+
+    temp = new_StringVar(uid, notes_val, 'notes', notes_callback)
+    notes_box[uid] = tk.Entry(root, textvariable=notes_val[temp._name][0])
+    notes_box[uid].grid(row=index + 2, column=8 + col_offset)
+
+    temp = tk.StringVar()
+    due_val[temp._name] = (temp, uid)
+    due_box[uid] = DateEntry(root, textvariable=due_val[temp._name][0], allow_empty=True, width=12, background='darkblue', foreground='white', borderwidth=2)
+    due_val[temp._name][0].set('')
+    due_box[uid].delete(0, "end")
+    due_val[temp._name][0].trace('w', due_callback)
+    due_box[uid].grid(row=index + 2, column=9 + col_offset)
+
+    
+    last_activity_box[uid] = tk.Label(text=str(pd.Timestamp.now().strftime('%d-%m-%Y')))
+    last_activity_box[uid].grid(row=index + 2, column=10 + col_offset)
+
+    created_box[uid] = tk.Label(text=str(pd.Timestamp.now().strftime('%d-%m-%Y')))
+    created_box[uid].grid(row=index + 2, column=11 + col_offset)
+
+    
 
 #######################################################################################
 
@@ -128,7 +201,7 @@ def load_row(index, row, col_offset):
 
     temp = tk.StringVar()
     due_val[temp._name] = (temp, uid)
-    due_box[uid] = DateEntry(root, allow_empty=True, width=12, background='darkblue', foreground='white', borderwidth=2)
+    due_box[uid] = DateEntry(root, textvariable=due_val[temp._name][0], allow_empty=True, width=12, background='darkblue', foreground='white', borderwidth=2)
     if pd.isnull(row['due']):
         due_val[temp._name][0].set('')
         due_box[uid].delete(0, "end")
@@ -143,8 +216,6 @@ def load_row(index, row, col_offset):
 
     created_box[uid] = tk.Label(text=str(row['created'].strftime('%d-%m-%Y')))
     created_box[uid].grid(row=index + 2, column=11 + col_offset)
-
-    # Have dictionaries based in ids rather than index
 
 
 def gui_to_df():
@@ -162,9 +233,6 @@ def load_json():
     print('load json')
     global df
     df = pd.read_json(filename, convert_dates=['due', 'last_activity', 'created'])     # TODO error handling
-    # df['due'] = pd.to_datetime(df['due'], unit='ns')
-    # df['last_activity'] = pd.to_datetime(df['last_activity'], unit='ns')
-    # df['created'] = pd.to_datetime(df['created'], unit='ns')
     for index, row in df.iterrows():
         load_row(index, row, 0)
 
@@ -173,6 +241,7 @@ if __name__ == "__main__":
     print("hello world")
 
     root = tk.Tk()
+    # root.configure(bg='white')
     root.geometry('1250x750')
     max_uid = 0
     date_entry = DateEntry(root)
@@ -200,7 +269,8 @@ if __name__ == "__main__":
     # title = tk.Label(text="Task Tracker by Project")
     # title.grid(column=0, row=0)
 
-    filename = r'C:\Users\brads\OneDrive\Programs\Task_Tracker_by_Project\example.json'
+    # filename = r'C:\Users\brads\OneDrive\Programs\Task_Tracker_by_Project\example.json'
+    filename = r'C:\Users\brads\OneDrive\Programs\Task_Tracker_by_Project\example2.json'
 
 
     status_values = ['Unknown', 'Open', 'In-process', 'Waiting on Responce', 'Closed']
@@ -237,65 +307,6 @@ if __name__ == "__main__":
         print(filename + " Not Acceptable")
 
     # clean_df()    
-
-    # for index, row in df.iterrows():
-    #     load_row(index, row, 0)
-
-        # # if row['status'] != Closed
-        # id_box[id] = tk.Label(root, text=str(index))
-        # id_box[id].grid(row=index + 2, column=0)
-
-        # # name_box = tk.Text(root, row['name'], height=2, width=15)
-        # name_box[id] = tk.Entry(root)
-        # name_box[id].insert(0, row['name'])
-        # name_box[id].grid(row=index + 2, column=1)
-
-        # # status_box = tk.OptionMenu(root, row['status'], *status_values)
-        # sb_var[id] = tk.StringVar()
-        # status_box[id] = ttk.Combobox(root, width = 20, textvariable = sb_var[id])
-
-        # status_box[id]['values'] = status_values
-        # # status_box.current(1)                         # TODO For all combo boxes
-        # # status_box['values'] = ('Open', 'Closed')
-        # status_box[id].grid(row=index + 2, column=2)
-
-        # #  project_box = tk.OptionsMenu(root, row['project'], *project_values)
-        # pb_var = tk.StringVar()
-        # project_box = ttk.Combobox(root, width = 20, textvariable = pb_var)
-
-        # project_box['values'] = project_values
-
-        # project_box.grid(row=index + 2, column=4)
-
-
-
-        # # group_box = tk.OptionMenu(root, row['group'], *group_values)
-        # gb_var = tk.StringVar()
-        # group_box = ttk.Combobox(root, width = 20, textvariable = gb_var)
-
-        # group_box['values'] = group_values
-
-        # group_box.grid(row=index + 2, column=5)
-
-        # # people_box =                    # TODO 
-
-        # TODO notes do we want to make larger box
-
-        # notes_box[id] = tk.Entry(root)
-        # notes_box[id].insert(0, row['notes'])
-        # notes_box[id].grid(row=index + 2, column=7)
-
-        # due_box = tk.Label(text=str(row['due']))
-        # due_box.grid(row=index + 2, column=8)
-
-        # last_activity_box = tk.Label(text=str(row['last_activity']))
-        # last_activity_box.grid(row=index + 2, column=9)
-
-        # created_box = tk.Label(text=str(row['created']))
-        # created_box.grid(row=index + 2, column=10)
-
-#     trace(name_val )
-# name_val[id].trace('w', update_callback)
    
     insert_button = tk.Button(root, text="Insert New Task", command=insert_new_row)
     insert_button.grid(row=0, column=2)
@@ -317,3 +328,4 @@ if __name__ == "__main__":
     print(df.to_string())
 
     root.mainloop()
+
